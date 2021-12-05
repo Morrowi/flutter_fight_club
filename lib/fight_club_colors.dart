a975e7c4b2b1709a4bfe0e172a027cddaba2674d
Михаил Егоров <morrowi@ya.ru>
2021-12-05 22:48:30 +0300

    update 4

--- lib/fight_club_colors.dart
+++ lib/fight_club_colors.dart
@@ -9,7 +9,7 @@ class FightClubColors{
   static const blueButton = const Color.fromRGBO(28, 121, 206, 1);
   static const blackButton = const Color.fromRGBO(0, 0, 0, 0.87);
   static const darkGreyText = const Color.fromRGBO(22, 22, 22, 1);
-  static const whiteText = const Color.fromRGBO(255, 255, 255, 87);
-  static const darkPuper = const Color(0xFC5D1EA);
+  static const whiteText = const Color(0xDDFFFFFF);
+  static const darkPuper = const Color(0xFFC5D1EA);
 
 }
